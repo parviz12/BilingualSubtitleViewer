6aaf92a0d88be778270e5796d523e7f8209cdafe
parviz12 <80689728+parviz12@users.noreply.github.com>
2026-08-28 13:43:36 +0330

    Add Media3 player for V5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,34 +6,24 @@ plugins {
 android {
     namespace = "com.example.bilingualsubviewer"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.example.bilingualsubviewer"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "5.0"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
@@ -42,4 +32,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.media3:media3-exoplayer:1.8.0")
+    implementation("androidx.media3:media3-ui:1.8.0")
 }
